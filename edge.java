e7ff2597b96c799ba86a176a232a5c56ab20cfa6
maladi17 <maladi1747@gmail.com>
2016-11-27 17:27:31 +0200

    Add files via upload
    this is an edited edge

--- edge.java
+++ edge.java
@@ -1,22 +1,44 @@
 public class edge {
 
-	public  int vert;
-	public double weight;
-	public edge(int v, double w){ 
-		vert = v; 
-		weight = w; 
-	}
-	public void setPrice(double w){  
-		weight = w; 
-	}
-	public edge(edge edge){ 
-		vert = edge.vert; 
-		weight = edge.weight; 
-	}
+
+		public  int vert;
+		public double weight;
+		
+		/**
+		 * constructor of edge 
+		 * @param v
+		 * @param w
+		 */
+		
+		public edge(int v, double w){ 
+			System.out.println("h");
+			vert = v; 
+			weight = w; 
+		}
+		
+		/**
+		 * setter to price of edge
+		 * @param w
+		 */
+		public void setPrice(double w){  
+			weight = w; 
+		}
+		public edge(edge edge){ 
+			vert = edge.vert; 
+			weight = edge.weight; 
+		}
 
 	@Override
-	public String toString() {
-		return "edge [vert=" + vert + ", weight=" + weight + "]";
+	/**
+	 * toString function
+	 * @return string
+	 */
+		public String toString() {
+			return "edge [vert=" + vert + ", weight=" + weight + "]";
+		}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
