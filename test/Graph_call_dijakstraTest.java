0c8d93251524d2019a1f9049ad952f91235c4a4e
lior95 <lior.levi89@gmail.com>
2016-11-27 22:52:56 +0200

    test for code 1.0

--- test/Graph_call_dijakstraTest.java
+++ test/Graph_call_dijakstraTest.java
@@ -4,7 +4,12 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.sun.prism.impl.VertexBuffer;
+
 import src.Graph_call_dijakstra;
+
+
+
 
 public class Graph_call_dijakstraTest {
 
@@ -19,7 +24,13 @@ public class Graph_call_dijakstraTest {
 	
 	@Test
 	public void testStatisticOutput() {
-		
+		src.Vertex[] vertexList = new src.Vertex[3];
+		vertexList[0] = new src.Vertex(0, Double.POSITIVE_INFINITY);
+		vertexList[1] = new src.Vertex(1, Double.POSITIVE_INFINITY);
+		vertexList[2] = new src.Vertex(2, Double.POSITIVE_INFINITY);
+		String value = Graph_call_dijakstra.statisticOutput(vertexList);
+		String check =  " Tie diameter=-1 from 0 to -1 radius=-1 from 1 to -1";
+		assertEquals(value,check);
 	}
 	
 	@Test
